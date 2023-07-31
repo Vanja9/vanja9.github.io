4c12d09d6df84de2ebd0ca6df0b0621cdafe57a3
Vanjavolijeju <kostic.vanja2@gmail.com>
2023-07-31 10:33:30 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,29 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#"><img src="@/assets/airserbia.png" height="28"></a>  
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item ">
+    <a class="navbar-brand" href="#"><img src="@/assets/airserbia.png" height="28"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
 
-        <strong><router-link class="nav-link " to="/">Home</router-link></strong>
-      </li>
-      <li class="nav-item">
+
+        <li class="nav-item ">
+          <strong><router-link class="nav-link " to="/">Home</router-link></strong>
+        </li>
+        <li class="nav-item">
           <strong><router-link class="nav-link " to="/search">Search</router-link></strong>
         </li>
-      <li class="nav-item">
-        <strong><router-link class="nav-link " to="about">About</router-link></strong>
-
-        <router-link class="nav-link " to="/">Home</router-link>
-      </li>
-      <li class="nav-item">
-          <router-link class="nav-link " to="/search">Search</router-link>
+        <li class="nav-item">
+          <strong><router-link class="nav-link " to="about">About</router-link></strong>
         </li>
-      <li class="nav-item">
-        <router-link class="nav-link " to="about">About</router-link>
 
-      </li>
-      
-    </ul>
-  </div>
-</nav>
-  <div class="text-light"><router-view/></div>
+
+      </ul>
+    </div>
+  </nav>
+  <div class="text-light"><router-view /></div>
 
   <footer class="text-light text-center container mt-4"> &copy; {{ year }} Univerzitet Singidunum </footer>
 </template>
